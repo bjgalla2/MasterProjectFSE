98901e55f991f99b301805e63c1476d7270c618f
bjgalla2 <45400883+bjgalla2@users.noreply.github.com>
2024-03-27 22:13:56 -0700

    GUI homescreen buttons

--- sketch.js
+++ sketch.js
@@ -27,33 +27,39 @@ function switchToMM(){
   game2Button.show();
   game3Button.show();
   game4Button.show();
+  game5Button.show();
 }
 
 function setup() {
   createCanvas(400, 400);
   background(220);
-  menuButton = createButton('Home Page');
-  menuButton.position(0, 0);
+  menuButton = createButton('Home');
+  menuButton.position(310, 310);
   menuButton.mousePressed(switchToMM);
   menuButton.hide();
   
-  game1Button = createButton('Game 1');
-  game1Button.position(10, 50);
+  game1Button = createButton('Math');
+  game1Button.position(80, 250);
   game1Button.mousePressed(game1Setup);
   game1Button.show();
   
-  game2Button = createButton('Game 2');
-  game2Button.position(10, 100);
+  game2Button = createButton('Tracing');
+  game2Button.position(80, 300);
   game2Button.mousePressed(game2Setup);
   game2Button.show();
   
-  game3Button = createButton('Game 3');
-  game3Button.position(10, 150);
+  game3Button = createButton('Typing');
+  game3Button.position(250, 250);
   game3Button.mousePressed(game3Setup);
   game3Button.show();
   
-  game4Button = createButton('Game 4');
-  game4Button.position(10, 200);
+  game4Button = createButton('Puzzle');
+  game4Button.position(250, 300);
+  game4Button.mousePressed(game4Setup);
+  game4Button.show();
+
+  game4Button = createButton('Color Matching');
+  game4Button.position(140, 350);
   game4Button.mousePressed(game4Setup);
   game4Button.show();
 }
@@ -76,6 +82,9 @@ function draw() {
     case 4: 
       game4Draw();
       break;
+    case 5:
+      game5Draw();
+      break
   }
 }
 
@@ -83,7 +92,8 @@ function mainMenu(){
   background(220);
   
   fill('black');
-  text('Click an activity', 200, 200);
+  text('Brain Game', 160, 90);
+  
 }
 
 /*****
